b3c88f7729de6e3809f24325b7ffc67a6129d402
Abdullah Said <abdullahsaid6923@gmail.com>
2026-05-26 00:04:35 +0700

    Add Google Analytics integration with @next/third-parties package
    - Add @next/third-parties dependency (^16.2.6) to package.json
    - Import GoogleAnalytics component in root layout
    - Add GoogleAnalytics component with tracking ID G-61RBRCWCXK

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -42,6 +43,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <GoogleAnalytics gaId="G-61RBRCWCXK" />
     </html>
   );
 }
